2e316e901091c44c0f67adb1dd019661600138a2
Sandeep Reddy Goli <golisandeep3@gmail.com>
2014-10-26 18:49:30 -0400

    Update median_two_sorted_arrays.java

--- median_two_sorted_arrays.java
+++ median_two_sorted_arrays.java
@@ -1,3 +1,32 @@
+/*Time Complexity O(logn)*/
+public static void median(int a[],int b[],int l1,int r1,int l2,int r2)
+	{
+		if(r1-l1+1==2 || r2-l2+1==2)
+		{
+			int d = Math.max(a[l1],b[l2]);
+			int f = Math.min(a[r1],b[r2]);
+			System.out.println(a[l1]);
+			System.out.println((d+f)/2);
+			return;
+		}
+		int mid1  = (l1+r1)/2;
+		int mid2 = (l2+r2)/2;
+		if(a[mid1]==b[mid2])
+		{
+		System.out.println(a[mid1]);
+		return;
+		}
+		else if(a[mid1]>a[mid2])
+		{
+			median(a,b,l1,mid1,mid2,r2);
+		}
+		else
+		{
+			median(a,b,mid1,r1,l2,mid2);
+		}
+	}
+
+
 public int median(int a[],int b[])
 	{
 		int l = (a.length+b.length)/2;
